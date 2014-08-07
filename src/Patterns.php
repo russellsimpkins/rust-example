3a8f81e6d99b3da97807339bf3b2a79e13d46014
Russell Simpkins <russellsimpkins@gmail.com>
2014-08-07 11:33:50 -0400

    adjusted so that json encoding happens inside the Patterns class and throws exceptions if there's some bad json

--- src/Patterns.php
+++ src/Patterns.php
@@ -3,7 +3,7 @@
 namespace Example;
 
 /**
- * This is a place to define regex patterns for RestFul routes
+ * This is a place to define regex patterns for RESTFul routes
  */
 class Patterns {
 
@@ -13,7 +13,7 @@ class Patterns {
      * slashes e.g. \ must be \\\\ because there's a double decode
      * this data will be json_decoded to get a hash. 
      */
-    function getPatterns() {
+    static function getPatterns() {
         $patterns = <<<'PATTERNS'
         {
             "RE_DATE"            : "^(1[89][0-9]{2}|2[0-9]{3})[-](0[1-9]|1[012])[-](0[1-9]|[12][0-9]|3[01])$",
@@ -36,5 +36,38 @@ class Patterns {
 PATTERNS;
 
         return $patterns;
+    }
+
+    /**
+     * Take a json string and convert it to a PHP hash
+     * @return hash of patterns.
+     */
+    static function getPatternsHash() {
+        $p = self::getPatterns();
+        $hash = json_decode($p, true);
+        if (json_last_error() == 0) {
+            return $hash;
+        }
+        $m = null;
+        switch (json_last_error()) {
+            case JSON_ERROR_NONE:
+                break;
+            case JSON_ERROR_DEPTH:
+                $m = 'Maximum stack depth exceeded.';
+            case JSON_ERROR_STATE_MISMATCH:
+                $m = 'Underflow or the modes mismatch.';
+            case JSON_ERROR_CTRL_CHAR:
+                $m = 'Unexpected control character found.';
+            case JSON_ERROR_SYNTAX:
+                $m = 'Syntax error, malformed JSON.';
+            case JSON_ERROR_UTF8:
+                $m = 'Malformed UTF-8 characters, possibly incorrectly encoded.';
+            default:
+                $m = 'Unknown error.';
+        }
+
+        if (!empty($m)) {
+            throw new \Exception("JSON Parsing error: $m");
+        }
     }
 }
